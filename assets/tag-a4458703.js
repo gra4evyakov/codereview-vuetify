795d1f0c23a1a9efc4ba5e4b26cbd03e63edaf41
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-05 15:52:25 +0300

    Updates

---
+++ assets/tag-a4458703.js
@@ -0,0 +1,1 @@
+import{k as t}from"./index-6be6cdf3.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- assets/tag-c79fb6a7.js
+++
@@ -1,1 +0,0 @@
-import{k as t}from"./index-565e6b50.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
