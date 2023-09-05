7f8eec7a0995824b0fda1648778f143020f8cc50
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-05 13:01:36 +0300

    Updates

---
+++ assets/tag-214631e7.js
@@ -0,0 +1,1 @@
+import{k as t}from"./index-aeb0c13a.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
--- assets/tag-3253c0b0.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"./index-19ccf72e.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
