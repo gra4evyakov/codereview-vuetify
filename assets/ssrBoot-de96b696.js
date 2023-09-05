78d7c7b96418d49f8f2b7fbbc20e1589cf302d65
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-05 13:57:59 +0300

    Updates

---
+++ assets/ssrBoot-de96b696.js
@@ -0,0 +1,1 @@
+import{T as t,d as e,n as s,av as n}from"./index-2d798c4f.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
