16902381b202e86e07b82d768c43129206ba80e8
gra4evyakov <gra4evyakov@yandex.ru>
2023-09-12 19:06:29 +0300

    Updates

---
+++ assets/ssrBoot-8a48ee03.js
@@ -0,0 +1,1 @@
+import{I as t,j as e,d as s,aS as n}from"./index-7ade655a.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
--- assets/tag-dfbc4111.js
+++
@@ -1,1 +0,0 @@
-import{q as t}from"./index-d248d8ef.js";const o=t({tag:{type:String,default:"div"}},"tag");export{o as m};
